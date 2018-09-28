a9750d4a15ebdb1a69266ca028e3ecf509b292bc
Mr.True.China <mr.true.china@gmail.com>
2018-09-28 21:55:10 +0900

    01

---
+++ Assets/Scripts/PerlinNoise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PerlinNoise
+{
+    public static float[,] GeneratePerlinNoiseMap(int width, int height, float scale)
+    {
+        float[,] map = new float[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sampleX = x * scale;
+                float sampleY = y * scale;
+                map[x, y] = Mathf.PerlinNoise(sampleX, sampleY);
+            }
+        }
+
+        return map;
+    }
+}
