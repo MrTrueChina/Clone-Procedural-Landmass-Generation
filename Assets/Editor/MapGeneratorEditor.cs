70c466906cf79d7e04b2800dd2bda06945d6a9bc
Mr.True.China <mr.true.china@gmail.com>
2018-09-29 21:04:52 +0900

    4
    大概是第四个视频

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CanEditMultipleObjects]
+//[CanEditMultipleObjects]                        //允许多重编辑？允许编辑多个？总之遇到不能多重编辑的时候就加上他。似乎又遇不到了
 [CustomEditor(typeof(MapGenerator))]
 public class MapGeneratorEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        DrawDefaultInspector();
+
+        DrawGenerateButton();
+    }
+    void DrawGenerateButton()
+    {
         MapGenerator generator = (MapGenerator)target;
-
-        if (DrawDefaultInspector())
-        {
-            generator.GenerateMap();
-        }
-
-        if (GUILayout.Button("Generator"))
+        if (GUILayout.Button("Generate"))
         {
             generator.GenerateMap();
         }
--- Assets/Scripts/PerlinNoise.cs
+++ Assets/Scripts/PerlinNoise.cs
@@ -4,35 +4,68 @@ using UnityEngine;
 
 public static class PerlinNoise
 {
-    public static float[,] GeneratePerlinNoiseMap(int width, int height, float scale, int octaves, float persistence, float lacunarity)
+    //octaves：倍频    persistence：持续度    lacunarity：隙度
+    public static float[,] GeneratePerlinNoiseMap(int mapWidth, int mapHeight,int seed, float scale, int octaves, float persistence, float lacunarity, Vector2 offset)
     {
-        if (scale <= 0) scale = 0.000001f;
+        if (scale <= 0) scale = 0.0001f;
 
-        float[,] map = new float[width, height];
 
-        for (int y = 0; y < height; y++)
+        Vector2[] octavesOffsets = GetOctavesOffsets(octaves, seed);
+
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float[,] map = new float[mapWidth, mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
         {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < mapWidth; x++)
             {
-                float amplitude = 1;
-                float frequency = 1;
+                float amplitude = 1;        //振幅
+                float frequency = 1;        //频率
                 float noiseHeight = 0;
 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float sampleX = x / scale * frequency;
-                    float sampleY = y / scale * frequency;
+                    float sampleX = (x - mapWidth / 2f) / scale * frequency + octavesOffsets[i].x + offset.x;
+                    float sampleY = (y - mapHeight / 2f) / scale * frequency + octavesOffsets[i].y + offset.y;
                     float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1;
-                    noiseHeight = perlinValue * amplitude;
+                    noiseHeight += perlinValue * amplitude;
 
                     amplitude *= persistence;
                     frequency *= lacunarity;
                 }
 
+                if (noiseHeight > maxNoiseHeight)
+                    maxNoiseHeight = noiseHeight;
+                else if (noiseHeight < minNoiseHeight)
+                    minNoiseHeight = noiseHeight;
+
                 map[x, y] = noiseHeight;
             }
         }
+        
+        for (int y = 0; y < mapHeight; y++)
+            for (int x = 0; x < mapWidth; x++)
+                map[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, map[x, y]);   //Mathf.InverseLerp：反差值，计算第三个参数在前两个参数之间的比例值
 
         return map;
+    }
+
+    static Vector2[] GetOctavesOffsets(int octaves, int seed)
+    {
+        Vector2[] octavesOffsets = new Vector2[octaves];
+
+        System.Random prng = new System.Random(seed);
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000);
+            float offsetY = prng.Next(-100000, 100000);
+
+            octavesOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        return octavesOffsets;
     }
 }
